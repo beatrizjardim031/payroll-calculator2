b46a6c6f299e6693bb3cc3cb1e0571c9df3df5e1
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-21 15:47:03 -0500

    Feat: add file writer and buffered writer

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -6,20 +6,30 @@ public class Program {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.println("Enter the name of the file to process: ");
+        System.out.print("Enter the name of the file to process: ");
         String userInput = input.nextLine();
 
         try {
             FileReader fileReader = new FileReader(userInput);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
             String line = bufferedReader.readLine();
+
+            System.out.print("Enter the name of the payroll file to create: ");
+            userInput = input.nextLine();
+
+            FileWriter fileWriter = new FileWriter(userInput);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             while(line != null) {
                 Employee employee = loadEmployee(line);
+                writeEmployee(employee, bufferedWriter);
                 System.out.printf("%d | %-20s | $%.2f%n",employee.getEmployeeID(), employee.getName(), employee.getGrossPay());
                 line = bufferedReader.readLine();
             }
             bufferedReader.close();
+            bufferedWriter.close();
+
         } catch (IOException e) {
             System.out.println("Wrong file. Please try again.");
         }
@@ -32,7 +42,13 @@ public class Program {
         double hoursWorked = Double.parseDouble(tokens[2]);
         double payRate = Double.parseDouble(tokens[3]);
 
-       Employee employee = new Employee(employeeID, name, hoursWorked, payRate);
-       return employee;
+        return new Employee(employeeID, name, hoursWorked, payRate);
+    }
+    public static void writeEmployee(Employee employee, BufferedWriter bufferedWriter){
+        try {
+            bufferedWriter.write(String.format("%d | %-20s | $%.2f%n", employee.getEmployeeID(), employee.getName(), employee.getGrossPay()));
+        } catch (IOException e) {
+            System.out.println("Oh no! Something is wrong... Try again later.");
+        }
     }
 }
